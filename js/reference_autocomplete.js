82218ee54e68ddf6fd444dbcddfdc8386ffe3386
mikemccaffrey <github@mikemccaffrey.net>
2016-08-04 18:51:30 -0700

    Create rough js function to change autocomplete url for reference widgets

---
+++ js/reference_autocomplete.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Javascript for reference autocomplete field widget.
+ */
+ (function ($) {
+
+"use strict";
+
+Backdrop.behaviors.reference_autocomplete = {
+  attach: function (context) {
+    $('.reference-autocomplete-select').change( function() {
+      var entity_bundle = $(this).val();
+      var autocomplete_id = $(this).attr('id').replace('entity-bundle', 'target-id');
+      var autocomplete_url = $('#' + autocomplete_id + "-autocomplete").val();
+      var autocomplete_url_base = autocomplete_url.substr(0, autocomplete_url.indexOf('reference_autocomplete'));
+      $('#' + autocomplete_id + "-autocomplete").val(autocomplete_url_base + 'reference_autocomplete/' + entity_bundle);
+      $('#' + autocomplete_id + "-autocomplete").removeClass('autocomplete-processed');
+      $('#' + autocomplete_id).val('');
+      Backdrop.behaviors.autocomplete.attach(document);
+    });
+  }
+};
+
+})(jQuery);
